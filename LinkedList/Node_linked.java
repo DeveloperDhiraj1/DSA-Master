5250c2fa529961bffe057c394c1b6332273b3af8
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2026-02-14 19:20:42 +0530

    day-17

---
+++ LinkedList/Node_linked.java
@@ -0,0 +1,33 @@
+
+public class Node_linked {
+
+    static class Node {
+
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        System.out.println(a.val);
+        System.out.println(b.val);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
